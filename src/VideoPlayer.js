10dc41caf31cdb8ec40f07d79a7245cd513377ef
jungmin <batay@soongsil.ac.kr>
2022-08-21 18:40:30 +0900

    Refactor: 리팩토링

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -6,12 +6,12 @@ const VideoPlayer = () => {
 
   //url 받아오는거 추가해야됨
   const playList = [
-    { index: 1, url: "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20200821/733655/MOV000256297_700X466.mp4" },
-    { index: 2, url: "https://sldict.korean.go.kr/multimedia/multimedia_files/convert/20191016/628088/MOV000250671_700X466.mp4" },
+    "http://sldict.korean.go.kr/multimedia/multimedia_files/convert/20200821/733655/MOV000256297_700X466.mp4",
+    "https://sldict.korean.go.kr/multimedia/multimedia_files/convert/20191016/628088/MOV000250671_700X466.mp4",
   ];
 
-  const handleNextVideo = (video, playIndex) => {
-    if (playIndex === video.length - 1) {
+  const handleNextVideo = (videoList, playIndex) => {
+    if (playIndex === videoList.length - 1) {
       setPlayIndex(0);
     } else {
       setPlayIndex(playIndex + 1);
@@ -21,7 +21,7 @@ const VideoPlayer = () => {
   return (
     <div>
       <ReactPlayer
-        url={playList[playIndex].url}
+        url={playList[playIndex]}
         width="33rem"
         height="20rem"
         playing={true} // 자동 재생 on
