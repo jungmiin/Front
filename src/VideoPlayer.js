e37d3d9deaad98ff8dae14ba1f5d094c7fbc7b43
jungmin <batay@soongsil.ac.kr>
2022-08-23 20:21:16 +0900

    Feat: video player 한번만 재생되도록 구현

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -5,6 +5,7 @@ import DataHandler from "./DataHandler";
 const VideoPlayer = (props) => {
   const [playIndex, setPlayIndex] = useState(0);
   const [playList, updatePlayList] = useState(0);
+  const [allPlayed, setAllPlayed] = useState(false);
 
   //새문장을 입력해서 번역을 시도했을 때 안됨.. 새로고침해야됨
 
@@ -20,6 +21,7 @@ const VideoPlayer = (props) => {
   const handleNextVideo = (playList, playIndex) => {
     if (playIndex === playList.length - 1) {
       setPlayIndex(0);
+      setAllPlayed(true);
     } else {
       setPlayIndex(playIndex + 1);
     }
@@ -31,12 +33,13 @@ const VideoPlayer = (props) => {
         url={playList[playIndex]}
         width="33rem"
         height="20rem"
-        playing={true} // 자동 재생 on
+        playing={allPlayed ? false : true} // 자동 재생 on
         muted={true} // 자동 재생 on
         controls={true} // 플레이어 컨트롤 노출 여부
         light={false} // 플레이어 모드
         pip={true} // pip 모드 설정 여부
         onEnded={() => {
+          setAllPlayed(false);
           handleNextVideo(playList, playIndex);
         }} // 플레이어 끝났을 때 이벤트
       />
